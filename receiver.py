6ca5b8114dd26cd6de1446cf82ba90b120c22ded
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2023-10-30 11:51:10 +0530

    changes

--- receiver.py
+++ receiver.py
@@ -23,35 +23,10 @@ while(1):
     received_data = s.recv(1024)
     data = pickle.loads(received_data)
     try:
-        # data_to_insert = f"'{str(data[0][0])}','{str(data[0][1])}','{str(data[0][2])}'"
         data_to_insert = [data[0][0], data[0][1], str(data[0][2])]
-        insert_query = "INSERT INTO Pool (sender_country, receiver_country, amount) VALUES (%s, %s, %s)"
+        insert_query ="INSERT INTO Pool (sender_country, receiver_country, amount) VALUES (%s, %s, %s)"
         cursor.execute(insert_query, data_to_insert)
         connection.commit()
         print(f"Data i.e. {data_to_insert} was inserted")
     except Exception as e:
         pass
-        # print("Exception",e)
-        # print("Data", data)
-    # full_msg = ""
-    # new_msg = True
-    # while(1):
-    #     msg = s.recv(10)
-    #     if new_msg:
-    #         print(f"\nnew message length: {msg[:HEADERSIZE]}")
-    #         msglen = int(msg[:HEADERSIZE])
-    #         new_msg = False
-
-    #     full_msg += msg.decode('utf-8')
-
-    #     if len(full_msg) - HEADERSIZE == msglen:
-    #         print(full_msg[HEADERSIZE:])
-    #         # data_to_insert = ('4','000079a280ae6a52c9053e3dfc8fcaedceb9397f50ca6adda7be90a5b348cc7f','00002af91ce372d48bd05f794e833db5a41ff19f271f56b16bde4a4cce50cec7','bye','24066')
-    #         data_to_insert = ()
-    #         insert_query = "INSERT INTO Blockchain (sender_country, receiver_country, amount) VALUES (%s, %s, %s)"
-    #         # cursor.execute(insert_query, data_to_insert)
-    #         connection.commit()
-    #         print("full msg recvd\n")
-    #         new_msg = True
-    #         full_msg = ""
-# [[<User: sarthak>, <User: NGO2>, 'donor1@donor.com', 'donor1@donor.com', 2]]
